fb5b251aad242bdedb7e71c43922c1ccc5737eb0
libangyao <libangyao@youzan.com>
2021-03-29 13:51:27 +0800

    chore: 调整打包目录

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  devServer: {
+    port: 23000
+  },
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include
+        .add('/packages')
+        .end()
+      .use('babel')
+        .loader('babel-loader')
+  }
+}
